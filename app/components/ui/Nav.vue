1c19091468c63b0b18f4d87b225c482b49344bd7
Nguyen Tu Ba Dat <nguyentubadat833@gmail.com>
2026-05-02 09:31:48 +0000

    add about page

--- app/components/ui/Nav.vue
+++ app/components/ui/Nav.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="flex justify-between items-center gap-3">
     <div class="flex md:justify-start justify-center gap-1">
-      <UButton v-for="item in nav" :label="item.label" :icon="item.icon" color="neutral" variant="link"
-        @click="navigateTo(item.to)" />
+      <UButton v-for="item in nav" :label="item.label" :icon="item.icon" color="neutral" variant="link" @click="navigateTo(item.to)" />
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 const nav = [
-  { label: 'Home', icon: 'ic:outline-house', to: '/' },
-  { label: 'Contact', icon: 'ic:outline-email', to: '/contact' },
-]
+  { label: "Home", icon: "ic:outline-house", to: "/" },
+  { label: "About", icon: "ic:outline-info", to: "/about" },
+];
 </script>
 
 <style></style>
---
+++ app/pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex justify-center">
+    <img src="/images/about_1.jpg " />
+  </div>
+</template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: "About",
+});
+</script>
