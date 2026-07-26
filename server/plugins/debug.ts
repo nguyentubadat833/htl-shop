203dfea0df124d79aca20ea4ca26fab6aaf21e7b
Nguyen Tu Ba Dat <nguyentubadat833@gmail.com>
2026-07-26 16:50:33 +0700

    update

--- server/plugins/debug.ts
+++ server/plugins/debug.ts
@@ -1,8 +1,36 @@
-// server/plugins/debug-error.ts
 export default defineNitroPlugin((nitroApp) => {
-    nitroApp.hooks.hook("error", (error, { event }) => {
-        console.error("🔥 SERVER ERROR:");
-        console.error(error);
-        console.error(error?.stack); // 👈 quan trọng
-    });
+  nitroApp.hooks.hook("request", (event) => {
+    const idRequest = generateRequestId();
+
+    const requestURL = getRequestURL(event);
+    const method = event.method;
+
+    event.context.id = idRequest;
+
+    logInfoColor(`[${getLocaleTime()}] | [${idRequest}] | REQUEST        | [${method}] ${requestURL.pathname}`);
+  });
+
+  nitroApp.hooks.hook("error", (error, { event }) => {
+    if (event) {
+      let errorInfo = `[${getLocaleTime()}] | [${event.context.id}] | RESPONSE ERROR`;
+
+      const userAuth = event.context.userAuth as UserAuth;
+      if (userAuth) {
+        errorInfo = errorInfo + ` | [USER ${userAuth.email}]`;
+      }
+
+      errorInfo = errorInfo + ` | [ERROR ${error.message ?? error.name ?? error.stack?.slice(0, 50)}]}`;
+      logErrorColor(errorInfo);
+    }
+  });
+
+  nitroApp.hooks.hook("afterResponse", (event) => {
+   let message = `[${getLocaleTime()}] | [${event.context.id}] | RESPONSE OK   `;
+   
+     const userAuth = event.context.userAuth as UserAuth;
+     if (userAuth) {
+       message = message + ` | [USER ${userAuth.email}]`;
+     }  
+     logInfoColor(message);
+  });
 });
--- server/utils/define-wrapped.ts
+++ server/utils/define-wrapped.ts
@@ -1,24 +1,8 @@
 import type { EventHandler, EventHandlerRequest } from "h3";
 import { getStatusMessage } from "./error";
-import { H3Event } from "h3";
-import { getLocaleTime } from "./generate-values";
-import { logErrorColor, logInfoColor } from "./log-helpers";
-import { UserAuth } from "./context-working";
 
-function handlerError(event: H3Event, err: unknown) {
-  const request = getRequestURL(event);
-
-  let errorInfo = `[${getLocaleTime()}] | [${event.context.id}] | RESPONSE ERROR`;
-
-  const userAuth = event.context.userAuth as UserAuth;
-  if (userAuth) {
-    errorInfo = errorInfo + ` | [USER ${userAuth.email}]`;
-  }
-
+function handlerError(err: unknown) {
   if (err instanceof ServerError) {
-    errorInfo = errorInfo + ` | ${err.message}`;
-    logErrorColor(errorInfo);
-
     throw createError({
       statusCode: err.code,
       statusMessage: err.message ?? getStatusMessage(err.code),
@@ -26,32 +10,18 @@ function handlerError(event: H3Event, err: unknown) {
     });
   }
 
-  logErrorColor(errorInfo);
-
   throw createError({
     statusCode: 500,
     statusMessage: getStatusMessage(500),
   });
 }
 
-function handlerResponse(event: H3Event) {
-  let message = `[${getLocaleTime()}] | [${event.context.id}] | RESPONSE OK   `;
-
-  const userAuth = event.context.userAuth as UserAuth;
-  if (userAuth) {
-    message = message + ` | [USER ${userAuth.email}]`;
-  }
-  logInfoColor(message);
-}
-
 export const defineWrappedResponseHandler = <T extends EventHandlerRequest, D>(handler: EventHandler<T, D>): EventHandler<T, D> =>
   defineEventHandler<T>(async (event) => {
     try {
-      const response = await handler(event);
-      handlerResponse(event);
-      return response;
+      return handler(event);
     } catch (err) {
-      handlerError(event, err);
+      handlerError(err);
     }
   });
 
@@ -63,12 +33,11 @@ export const defineWrappedRequiredAuthHandler = <T extends EventHandlerRequest, 
         statusCode: 401,
       });
     }
+
     try {
-      const response = await handler(event);
-      handlerResponse(event);
-      return response;
+      return handler(event);
     } catch (err) {
-      handlerError(event, err);
+      handlerError(err);
     }
   });
 
@@ -80,13 +49,12 @@ export const defineWrappedRequiredAdminHandler = <T extends EventHandlerRequest,
         statusCode: 401,
       });
     }
+
     try {
       userContextService.hasAdminOrThrow();
 
-      const response = await handler(event);
-      handlerResponse(event);
-      return response;
+      return handler(event);
     } catch (err) {
-      handlerError(event, err);
+      handlerError(err);
     }
   });
